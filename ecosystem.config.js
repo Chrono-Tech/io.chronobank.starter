46fe168de0d785f14f80ab4dded875190d8cdc6c
Igor Pavlenko <ipavlenko@inbox.ru>
2017-11-27 02:01:13 +0500

    Uploads dir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'COOKIE_SECRET': 'dfcc1c155c0e392de21af2a94ac9c25e0c5833ba8a8e6e4f79a937b0995b31cd2a924c863de694b6a229be62920b10e6509b9ec85b7e7c0b99c3d34a244b965b',
         'CLOUDINARY_URL': 'cloudinary://535824645681574:s0SGkP29qSKSV5vn9hwwla_GCzY@duxucxxwd',
         'NODE_ENV': 'development',
+        'UPLOAD_DIR': '/home/pavlenko/Projects/ChronoWeb/io.chronobank.uploads',
         'MONGO_URI': 'mongodb://localhost:27017/backend-chronobank-io',
         'SOLR_URI': 'http://solr.chronobank.tp.ntr1x.com/solr/io-chronobank'
       },
@@ -18,6 +19,7 @@ module.exports = {
         'COOKIE_SECRET': 'dfcc1c155c0e392de21af2a94ac9c25e0c5833ba8a8e6e4f79a937b0995b31cd2a924c863de694b6a229be62920b10e6509b9ec85b7e7c0b99c3d34a244b965b',
         'CLOUDINARY_URL': 'cloudinary://535824645681574:s0SGkP29qSKSV5vn9hwwla_GCzY@duxucxxwd',
         'NODE_ENV': 'production',
+        'UPLOAD_DIR': '/var/data/io.chronobank.uploads',
         'MONGO_URI': 'mongodb://localhost:27017/backend-chronobank-io',
         'SOLR_URI': 'http://solr.chronobank.tp.ntr1x.com/solr/io-chronobank'
       }
